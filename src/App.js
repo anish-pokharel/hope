000bbe4746ceec826df52ae6843aee8a45bf9fdb
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-26 23:24:51 +0545

    Navbar html complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
+      <Home />
     </>
   );
 }
