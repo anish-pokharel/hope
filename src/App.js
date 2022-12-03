da18fa19f6706cff20e5bad16cbb7c062afff194
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-04 01:05:40 +0545

    header head contact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter> */}
       <Header />
-      <Navbar />
-      <Home />
       <Contact />
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Head from "./Head";
 import "./Header.css";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+
   return (
     <>
-      <section className="header ">
-        <div className="container headerflex ">
-          <div className="left">
-            <div className="icons">
-              <i className="fab fa-facebook-f"></i>
-              <i className="fab fa-instagram"></i>
-              <i className="fab fa-twitter"></i>
-            </div>
-          </div>
-          <div className="right">
-            <div className="icons">
-              <i className="fas fa-phone">
-                <span>1300424442</span>
-              </i>
-              <i class=" fas fa-file">
-                <span>Important documents</span>
-              </i>
-              <i class="fas fa-user-plus">
-                <span>Referrals</span>
-              </i>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Head />
+      <header>
+        <nav className="flexSB">
+          <ul
+            className={click ? "mobile-nav" : "flexSB"}
+            onClick={() => setClick(false)}
+          >
+            <li>
+              <a to="/">Home</a>
+            </li>
+            <li>
+              <a to="/courses">All Courses</a>
+            </li>
+            <li>
+              <a to="/about">About</a>
+            </li>
+            <li>
+              <a to="/team">Team</a>
+            </li>
+            <li>
+              <a to="/pricing">Pricing</a>
+            </li>
+            <li>
+              <a to="/blog">Blog</a>
+            </li>
+
+            <li>
+              <a to="/contact">Contact</a>
+            </li>
+          </ul>
+          <button className="toggle" onClick={() => setClick(!click)}>
+            {click ? (
+              <i className="fa fa-times"></i>
+            ) : (
+              <i className="fa fa-bars"></i>
+            )}
+          </button>
+        </nav>
+      </header>
     </>
   );
 };
