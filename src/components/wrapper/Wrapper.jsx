7fe57ee92dcd037c133a2153117593352cd36d06
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-05 23:02:18 +0545

    wrapper

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -19,21 +19,6 @@ const Wrapper = () => {
           </div>
         </div>
       </section>
-      {/* <section className="container">
-        <div className="flexSB  ">
-          <div className="reg">
-            <h2>We Are Registered NDIS Provider</h2>
-            <button className="reg-btn"> Refer a participany</button>
-          </div>
-          <div className="advice">
-            <h2>Looking for Advice?</h2>
-            <p>
-              Call us on 1300 424 442 to talk about your needs. Or fill out the
-              form below and our Care Advisors will be in touch soon.
-            </p>
-          </div>
-        </div>
-      </section> */}
     </>
   );
 };
