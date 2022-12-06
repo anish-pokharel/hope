df4dd0d2a7179886fd47d8aaad0d45d8c8c981cc
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-06 22:41:05 +0545

    footer complete

--- src/components/footer/Fotter.jsx
+++ src/components/footer/Fotter.jsx
@@ -4,9 +4,45 @@ import "./Fotter.css";
 const Fotter = () => {
   return (
     <>
-      <footer>
+      <section id="foot">
         <div className="flexMI footer">
-          <div className="right">
+          <div className="describe">
+            <h2>Hope Ability</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim
+              doloribus, mollitia soluta ipsa doloremque et explicabo facilis
+              eveniet tempora dolorum esse aliquam totam neque minus?
+            </p>
+          </div>
+          <div className="linked">
+            <div className="link1">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Our Service</li>
+                <li>Registered</li>
+                <li>Refferals</li>
+              </ul>
+            </div>
+            <div className="link2">
+              <ul>
+                <li>Contact</li>
+                <li>Work With US</li>
+                <li>Documents</li>
+                <li>Blog</li>
+              </ul>
+            </div>
+            <div className="iconsdesc">
+              <li>
+                <i className="fab fa-facebook-f"></i>
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-linkedin"></i>
+              </li>
+            </div>
+          </div>
+
+          {/* <div className="right">
             <h2>Hope Ability</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
@@ -40,12 +76,12 @@ const Fotter = () => {
                 <i className="fab fa-linkedin"></i>
               </li>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="footerright">
           <p>© 2021. All rights reserved by ADK</p>
         </div>
-      </footer>
+      </section>
     </>
   );
 };
