daf8eb15a0b4d884e40a8cee9e5a1aa5ee13f9c8
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-06 19:10:10 +0545

    mobile view

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import "./Wrapper.css";
+// import "./Wrapper.css";
 
 const Wrapper = () => {
   return (
     <>
-      <section className="wrapper">
-        <div className="grid">
-          <div className="reg">
-            <h3>We Are Registered NDIS Provider</h3>
-            <button>Refer a participant</button>
-          </div>
-          <div className="advice">
-            <h3>Looking For Advice?</h3>
-            <p>
-              Call us on 1300 424 442 to talk about your needs. Or fill out the
-              form below and our Care Advisors will be in touch soon.
-            </p>
-          </div>
+      <div className="flexMI wrapper container">
+        <div className="reg">
+          <h3>We Are Registered NDIS Provider</h3>
+          <button>Refer a participant</button>
         </div>
-      </section>
+        <div className="advice">
+          <h3>Looking For Advice?</h3>
+          <p>
+            Call us on 1300 424 442 to talk about your needs. Or fill out the
+            form below and our Care Advisors will be in touch soon.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
