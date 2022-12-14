2e4cf252a64f5ad68b8911ed4f00dd003e91c171
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-14 23:41:30 +0545

    trust

--- src/components/trust/Trust.jsx
+++ src/components/trust/Trust.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Title from "../common/Title";
 import "./Trust.css";
-
+import about from "../../image/about.jpg";
 const Trust = () => {
   return (
     <>
       <section className="container">
         <Title title="We're All About Trust" />
         <div className="flexSB">
-          <div className="left row"></div>
-          <div className="right row"></div>
+          <div className="left row">
+            <div className="aboutimg">
+              <img src={about} alt="" />
+            </div>
+          </div>
+          <div className="right row">
+            <div className="para">
+              <p>
+                Hope Ability management has years of experience in disability
+                services along with extensive experience in customer focused
+                public and private sector. Our vision is to empower participants
+                to take ownership of their life.
+              </p>
+            </div>
+            <div className="button">
+              <button>About Hope Ability </button>
+            </div>
+          </div>
         </div>
       </section>
     </>
