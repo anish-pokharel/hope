2bf1b47eb0652371fd61681ec37b7871d50a3da5
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-14 23:37:04 +0545

    flex error handle

--- src/components/trust/Trust.jsx
+++ src/components/trust/Trust.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Title from "../common/Title";
+import "./Trust.css";
 
 const Trust = () => {
   return (
     <>
       <section className="container">
         <Title title="We're All About Trust" />
-        <div className="flexSB"></div>
+        <div className="flexSB">
+          <div className="left row"></div>
+          <div className="right row"></div>
+        </div>
       </section>
     </>
   );
