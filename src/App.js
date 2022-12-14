0b7a81dea401fec7f273295e9325ee0cac30cfa1
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-14 22:43:16 +0545

    page routing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import "./App.css";
-import Contact from "./components/contact/Contact";
-import Header from "./components/header/Header";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./components/home/Home";
-// import { Route, Routes, BrowserRouter } from "react-router-dom";
-
-import Mission from "./components/mission/Mission";
-import Wrapper from "./components/wrapper/Wrapper";
-import Service from "./components/service/Service";
-import Fotter from "./components/footer/Fotter";
+import Page from "./components/Page";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/service" element={<Service />} />
-          <Route path="/mission" element={<Mission />} />
-          <Route path="/wrapper" element={<Wrapper />} />
-          {/* <Service />
-          <Mission />
-          <Wrapper /> */}
-
-          {/* <Contact /> */}
-        </Routes>
-        <Fotter />
-      </BrowserRouter>{" "}
-      {/* <Header /> */}
-      {/* <Home/> */}
-      {/* <Service /> */}
-      {/* <Mission /> */}
-      {/* <Wrapper /> */}
-      {/* <Contact /> */}
+      <Page />
     </>
   );
 }
--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Fotter from "./footer/Fotter";
+import Header from "./header/Header";
+import Mission from "./mission/Mission";
+import Service from "./service/Service";
+import Wrapper from "./wrapper/Wrapper";
 
-const page = () => {
-  return <></>;
+const Page = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/service" element={<Service />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/wrapper" element={<Wrapper />} />
+        </Routes>
+        <Fotter />
+      </BrowserRouter>
+    </>
+  );
 };
 
-export default page;
+export default Page;
