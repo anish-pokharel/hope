fe356a80eb8cedf47db2e3f92fc072032c04d56f
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-14 22:57:24 +0545

    Home started

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Fotter from "./footer/Fotter";
 import Header from "./header/Header";
+import Home from "./home/Home";
 import Mission from "./mission/Mission";
 import Service from "./service/Service";
+import Trust from "./trust/Trust";
 import Wrapper from "./wrapper/Wrapper";
 
 const Page = () => {
@@ -12,6 +14,8 @@ const Page = () => {
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trust" element={<Trust />} />
           <Route path="/service" element={<Service />} />
           <Route path="/mission" element={<Mission />} />
           <Route path="/wrapper" element={<Wrapper />} />
---
+++ src/components/trust/Trust.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trust = () => {
+  return (
+    <>
+      <h1>hello from trust</h1>
+    </>
+  );
+};
+
+export default Trust;
